3db4c9b0eeac33e210f6bf63a3b3c3049c9683dd
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-03-18 01:31:30 +0200

    test

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, c;
+
+	for (i = 0, c = 'a'; i < 16; i++)
+	{
+		if (i > 9)
+			putchar(c++);
+		else
+			putchar(i);
+	}
+	putchar('\n')
+	return (0);
+}
