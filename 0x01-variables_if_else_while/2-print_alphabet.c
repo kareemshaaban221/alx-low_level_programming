002e903812f51002db4be8bd400c33adeb895c81
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-03-18 01:02:45 +0200

    test

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'main function'
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	for (char c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
