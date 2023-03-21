c88cb2fc0e53c47d7a08736c972237bd7515f8b7
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-03-21 23:21:03 +0200

    test

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'main function'
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	long n1 = 1, n2 = 2, tmp, sum = 2;
+
+	int i;
+
+	while (n1 + n2 <= 4000000)
+	{
+		tmp = n1 + n2;
+		if (tmp % 2 == 0)
+			sum += tmp;
+		n1 = n2;
+		n2 = tmp;
+	}
+	printf("%ld\n", sum);
+
+	return (0);
+}
