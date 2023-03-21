c106bebb23fe6868014e6254b9d0c25fa187633e
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-03-21 17:57:19 +0200

    test

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,16 +3,13 @@
 /**
  * _islower - Entry point
  * Description: 'main function'
+ * @c: Description int char
  * Return: 0 (success)
  */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
-	{
-		return 1;
-	}
+		return (1);
 	else
-	{
-		return 0;
-	}
+		return (0);
 }
