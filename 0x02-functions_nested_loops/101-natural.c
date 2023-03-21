490eb52f0bc216126ea6eede17ddc2350807142b
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-03-21 22:33:18 +0200

    test

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'main function'
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 3; i < 1024; i += 3)
+	{
+		sum += i;
+	}
+
+	for (i = 5; i < 1024; i += 5)
+	{
+		sum += i;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
