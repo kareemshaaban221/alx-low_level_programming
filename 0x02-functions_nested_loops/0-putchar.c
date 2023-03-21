173fc97140e7aee72d3b3c7ec99907a1c986c06a
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-03-21 16:50:07 +0200

    test

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "_putchar.c"
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Description: 'main function'
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char str[] = "_putchar";
+
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i++]);
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
