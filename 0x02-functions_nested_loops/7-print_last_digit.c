a42e3a2e78df9fa2174cfcf9c795a55c9fddeabb
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-03-21 19:38:27 +0200

    test

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ * Description: 'main function'
+ * @n: integer
+ * Return: integer (success)
+ */
+int print_last_digit(int n)
+{
+	int last;
+
+	last = n % 10;
+	_putchar('0' + k);
+
+	return (k);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -13,4 +13,5 @@ int _putchar(char);
 /* int _islower(int); */
 /* int _isalpha(int); */
 /* int print_sign(int); */
-int _abs(int);
+/* int _abs(int); */
+int print_last_digit(int);
