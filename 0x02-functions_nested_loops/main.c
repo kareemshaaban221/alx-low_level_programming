b53b18f2c56939c14b48df0c7070fd60274649c4
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-03-21 17:32:48 +0200

    test

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: 'main function'
+ * Return: 0 (success)
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
