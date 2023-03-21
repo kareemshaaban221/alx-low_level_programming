e883032d367c4f800b52d58f3cf883264864887a
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-03-21 21:17:04 +0200

    test

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * Description: 'main function'
+ * @n1: normal integer
+ * @n2: normal integer
+ * Return: int (success)
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
