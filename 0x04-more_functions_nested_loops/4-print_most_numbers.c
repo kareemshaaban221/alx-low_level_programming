b588038978cb19bf1e41628d205bd8026e8aee46
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-03-24 01:51:12 +0200

    test

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (c == '2' || c == '4')
+			continue;
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
