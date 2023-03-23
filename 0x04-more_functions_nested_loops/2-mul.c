987cff9ff67edd549ada42251bf06ebb2067a1e1
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-03-24 01:42:59 +0200

    test

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * _isupper - check the code for Holberton School students.
+ * mul - check the code for Holberton School students.
  * @a: char
  * @b: djgs
  * Return: Always 0.
