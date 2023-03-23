9fccd2780912ea8db3ff778066c54a1b65f5292e
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-03-24 01:41:24 +0200

    test

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isupper - check the code for Holberton School students.
+ * @c: char
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
