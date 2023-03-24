61c14898d8c41c70064a0b34bc32659fa42e27e6
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-03-24 02:39:17 +0200

    test

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagonal - check the code for Holberton School students.
+ * @n: char
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+	if (n > 0)
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n')
+		}
+	}
+	else
+		_putchar('\n');
+}
