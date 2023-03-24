62666876207422b127d7bf42735488cfa1a2146d
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-03-24 04:57:47 +0200

    test

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,4 +1,70 @@
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+#define push(sp, n) (*((sp)++) = (n))
+#define pop(sp) (*--(sp))
+
+char *sumBigInt(char *n1, char *n2)
+{
+	char *res;
+
+	int stack[1000];
+
+	int *sp;
+
+	int l1 = strlen(n1), l2 = strlen(n2), minlen, maxlen, i, j, rem, tmp1, tmp2, tmpres, reslen;
+
+	sp = stack;
+
+	minlen = min(l1, l2);
+	maxlen = max(l1, l2);
+
+	for (i = minlen - 1, j = maxlen - 1, rem = 0; i >= 0; i--, j--) {
+		if(l1 > l2)
+		{
+			tmp1 = n1[j] - '0';
+			tmp2 = n2[i] - '0';
+		}
+		else
+		{
+			tmp2 = n2[j] - '0';
+			tmp1 = n1[i] - '0';
+		}
+		tmpres = tmp1 + tmp2 + rem;
+
+		rem = tmpres / 10;
+		push(sp, tmpres % 10);
+	}
+
+	if (l1 > l2)
+	{
+		while (j >= 0)
+		{
+			tmp1 = n1[j--] - '0';
+			tmp1 += rem;
+			rem = tmp1 / 10;
+			push(sp, tmp1);
+		}
+	}
+	else if (l2 > l1)
+	{
+		while (j >= 0)
+		{
+			tmp2 = n2[j--] - '0';
+			tmp2 += rem;
+			rem = tmp2 / 10;
+			push(sp, tmp2);
+		}
+	}
+
+	if (rem > 0) 
+		push(sp, rem);
+
+	reslen = sp - stack;
+
+	
+}
 
 /**
  * main - Entry point
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,15 +8,15 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char);
-/* void print_alphabet(void); */
-/* void print_alphabet_x10(void); */
-/* int _islower(int); */
-/* int _isalpha(int); */
-/* int print_sign(int); */
-/* int _abs(int); */
-/* int print_last_digit(int); */
-/* void jack_bauer(void); */
-/* void times_table(void); */
-/* int add(int, int); */
-/* void print_to_98(int n); */
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int);
+int _isalpha(int);
+int print_sign(int);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int);
 void print_times_table(int);
