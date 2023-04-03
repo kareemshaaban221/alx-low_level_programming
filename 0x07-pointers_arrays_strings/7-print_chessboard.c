7774bfde0bcee2e1848e9844147c416647ea2ef9
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-04-03 13:12:31 +0200

    test

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,16 +8,18 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	int i;
+	int i, j;
 	char *s;
 
 	for (i = 0; i < 8; i++)
 	{
 		s = a[i];
-		while (*s != '\0')
+		j = 0;
+		while (j < 8)
 		{
 			_putchar(*s);
 			s++;
+			j++;
 		}
 		_putchar('\n');
 	}
