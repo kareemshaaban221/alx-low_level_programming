c61c15085490f8eae11731df3346d83222b070f2
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-04-03 11:50:49 +0200

    test

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - function
+ * @s: pointer
+ * Return: char *
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ss = s;
+
+	while (*ss != '\0' && n--)
+	{
+		*ss = b;
+		ss++;
+	}
+
+	return (s);
+}
