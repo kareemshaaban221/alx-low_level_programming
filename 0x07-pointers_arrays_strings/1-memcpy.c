1af8415ced0fa08b1698958bc44090a424530827
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-04-03 12:07:04 +0200

    test

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memset - function
+ * @dest: pointer
+ * @src: char
+ * @n: int
+ * Return: char *
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *d = dest, s = src;
+
+	while (n--)
+	{
+		*d = *s;
+		d++;
+		s++;
+	}
+
+	return (dest);
+}
