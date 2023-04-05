595dff4b90ef1ab95874a1bf5731a7e1d2d532ed
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-04-05 23:19:44 +0200

    test

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_puts_recursion - Prints a string using recursion
+ *@s: Pointer to string
+ *Return: void
+ */
+void _puts_recursion(char *s)
+{
+char *ss = s;
+
+if (*ss == '\0')
+return;
+
+_putchar(*ss);
+_puts_recursion(++ss);
+
+}
