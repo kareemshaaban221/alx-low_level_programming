c230cbb90f8c22053dff83a8bcf1262f25cc37c9
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-04-10 15:46:17 +0200

    test

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: count of cli arguments
+ * @argv: command line arguments
+ * Return: integer
+*/
+int main(int argc, char **argv)
+{
+if (argc > 0)
+{
+printf("%s\n", argv[0]);
+}
+
+return (0);
+}
