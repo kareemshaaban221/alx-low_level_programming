f5600c55cd6d3c9a684600ec0722851ac6c08a1e
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-04-10 16:59:08 +0200

    test

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,46 +1,5 @@
 #include <stdio.h>
-
-/**
- * _strlen - function
- * @s: pointer
- * Return: int len
- */
-int _strlen(char *s)
-{
-int len = 0;
-
-while (*s != '\0')
-{
-len++;
-s++;
-}
-
-return (len);
-}
-
-/**
- * convert - Entry point
- * Description: 'main function'
- * @s: normal integer
- * Return: res (success)
- */
-int convert(char *s)
-{
-int w = 1;
-int res = 0;
-int len = _strlen(s);
-int i;
-int tmp = 0;
-
-for (i = len - 1; i >= 0; i--)
-{
-tmp = s[i] - '0';
-res = res + (tmp *w);
-w *= 10;
-}
-
-return (res);
-}
+#include <stdlib.h>
 
 /**
  * main - entry point
@@ -56,7 +15,7 @@ printf("Error\n");
 return (1);
 }
 
-printf("%d\n", convert(argv[1]) * convert(argv[2]));
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 
 return (0);
 }
