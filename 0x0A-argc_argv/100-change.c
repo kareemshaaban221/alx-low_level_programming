90a7862db034e73d8613c4b8087deb4319881b48
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-04-10 17:33:16 +0200

    test

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,32 +10,32 @@
 */
 int main(int argc, char **argv)
 {
-    int res = 0;
-    int n = 0;
-    int coins[] = {25, 10, 5, 2, 1};
-    int i = 0;
+int res = 0;
+int n = 0;
+int coins[] = {25, 10, 5, 2, 1};
+int i = 0;
 
-    if (argc != 2)
-    {
-        printf("Error\n");
-        return (1);
-    }
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
 
-    n = atoi(argv[1]);
+n = atoi(argv[1]);
 
-    if (n < 0)
-    {
-        printf("0\n");
-        return (0);
-    }
+if (n < 0)
+{
+printf("0\n");
+return (0);
+}
 
-    for (i = 0; i < 5; i++)
-    {
-        res += n / coins[i];
-        n %= coins[i];
-    }
+for (i = 0; i < 5; i++)
+{
+res += n / coins[i];
+n %= coins[i];
+}
 
-    printf("%d\n", res);
+printf("%d\n", res);
 
-    return (0);
+return (0);
 }
