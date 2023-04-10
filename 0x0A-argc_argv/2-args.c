d91fbe88354745b20cf415ebe926e5dbc8f0b2bc
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-04-10 15:56:32 +0200

    test

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: count of cli arguments
+ * @argv: command line arguments
+ * Return: integer
+*/
+int main(int argc, char **argv)
+{
+if (argc > 0)
+{
+    while (argc--)
+    {
+        printf("%s\n", *argv);
+        argv++;
+    }
+}
+
+return (0);
+}
