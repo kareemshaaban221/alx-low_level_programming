3eb76c8492a15e90a667e9cc6fea4f096da71377
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-04-12 16:30:23 +0200

    test

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -34,6 +34,9 @@ char *res;
 int len = _strlen(s);
 int i;
 
+if (str == NULL)
+return (NULL);
+
 if (len <= 0)
 {
 res = malloc(1);
@@ -41,13 +44,15 @@ res[0] = '\0';
 return (res);
 }
 
-res = malloc(sizeof(char) * len);
+res = malloc(sizeof(char) * (len + 1));
 
 if (res == NULL)
 return (NULL);
 
 for (i = 0; i < len; i++)
 res[i] = s[i];
+
+res[len] = '\0';
 
 return (res);
 }
