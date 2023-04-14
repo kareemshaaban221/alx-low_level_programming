f952d7d64c975d9ef17c014ec95957ff5910d837
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-04-14 04:15:52 +0200

    test

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -20,8 +20,8 @@ ptr = malloc(nmemb * size);
 if (ptr == NULL)
 return (NULL);
 
-for (i = 0; i < nmemb; i++)
-*(ptr + i) = 0;
+for (i = 0; i < nmemb * size; i++)
+*(ptr + i) = '\0';
 
 return (ptr);
 }
