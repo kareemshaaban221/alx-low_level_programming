f1ad00b9f40744450226c03895ceab39982a2320
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-04-17 19:39:25 +0200

    test

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct for dog
+ * struct dog
+ * Description: struct for dog
+ * @name: name of dog
+ * @owner: owner of dog
+ * @age: age of dog
+*/
+struct dog
+{
+char *name;
+char *owner;
+float age;
+};
+
+#endif
