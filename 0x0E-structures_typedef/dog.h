e2367d42c6afcc4ca31a5a66b4002262932a4501
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-04-17 19:59:28 +0200

    test

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,13 +9,14 @@
  * @owner: owner of dog
  * @age: age of dog
 */
-typedef struct dog dog_t;
 struct dog
 {
 char *name;
 char *owner;
 float age;
 };
+
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
