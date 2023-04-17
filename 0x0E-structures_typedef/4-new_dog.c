a4c583d4e32ce2fe544d5578869e6250d014e71b
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-04-17 20:24:26 +0200

    test

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -21,28 +21,6 @@ return (len);
 }
 
 /**
- * _strcpy - function
- * @dest: pointer
- * @src: pointer
- * Return: char *
- */
-char *_strcpy(char *dest, char *src)
-{
-char *res = dest;
-
-while (*src != '\0')
-{
-*dest = *src;
-dest++;
-src++;
-}
-
-*dest = '\0';
-
-return (res);
-}
-
-/**
  * new_dog - create a new
  * @name: the name of the
  * @age: the age of the
@@ -52,31 +30,32 @@ return (res);
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *res = malloc(sizeof(dog_t));
-char *nname = malloc(sizeof(char) * _strlen(name));
-char *oowner = malloc(sizeof(char) * _strlen(owner));
+int i;
 
-if (!res || !name || !owner || !nname || !oowner)
+if (!res || !name || !owner)
 {
 free(res);
-free(nname);
-free(oowner);
 return (NULL);
 }
 
-res->name = _strcpy(nname, name);
-res->owner = _strcpy(oowner, owner);
-res->age = age;
+res->name = malloc(sizeof(char) * _strlen(name));
+res->owner = malloc(sizeof(char) * _strlen(owner));
 
 if (!res->name || !res->owner)
 {
 free(res);
-free(nname);
-free(oowner);
 return (NULL);
 }
 
-free(nname);
-free(oowner);
+for (i = 0; name[i] != '\0'; i++)
+res->name[i] = name[i];
+res->name[i] = '\0';
+
+for (i = 0; owner[i] != '\0'; i++)
+res->owner[i] = owner[i];
+res->owner[i] = '\0';
+
+res->age = age;
 
 return (res);
 }
