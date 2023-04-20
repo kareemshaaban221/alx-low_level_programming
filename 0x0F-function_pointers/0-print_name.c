349c5568f1d078850e469d282baa4c30665b1051
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-04-20 03:39:09 +0200

    test

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,12 @@
 #include "main.h"
 
+/**
+ * print_name - print the name
+ * @name: the name
+ * @f: the function
+ * Return: the value
+*/
 void print_name(char *name, void (*f)(char *))
 {
-    (*f)(name);
+(*f)(name);
 }
