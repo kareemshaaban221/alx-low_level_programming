c7ea5ec676d8b432bc476b4b2f076f6a7632f5d3
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-04-22 00:15:19 +0200

    test

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,15 +8,15 @@
  */
 int _strlen(char *s)
 {
-	int len = 0;
+int len = 0;
 
-	while (*s != '\0')
-	{
-		len++;
-		s++;
-	}
+while (*s != '\0')
+{
+len++;
+s++;
+}
 
-	return (len);
+return (len);
 }
 
 /**
@@ -37,7 +37,7 @@ va_end(args);
 */
 int inTarget(char c)
 {
-    return c == 'c' || c == 'i' || c == 'f' || c == 's' ? 1 : 0;
+return c == 'c' || c == 'i' || c == 'f' || c == 's' ? 1 : 0;
 }
 
 /**
@@ -47,40 +47,40 @@ int inTarget(char c)
 */
 void print_all(const char * const format, ...)
 {
-    unsigned int i, n;
-    char *tmp;
-    va_list args;
-    if (!format)
-    {
-        printf("\n");
-        return;
-    }
-    n = _strlen((char *) format);
-    va_start(args, format);
-    i = 0;
-    while (i < n)
-    {
-        if (i > 0 && inTarget(format[i]) == 1)
-            printf(", ");
-        switch (format[i])
-        {
-            case 'c':
-                printf("%c", va_arg(args, int));
-                break;
-            case 'i':
-                printf("%d", va_arg(args, int));
-                break;
-            case 'f':
-                printf("%f", va_arg(args, double));
-                break;
-            case 's':
-                tmp = va_arg(args, char *);
-                printf("%s", tmp == NULL ? "(nil)" : tmp);
-                break;
-            default:
-                break;
-        }
-        i++;
-    }
-    ending(args);
+unsigned int i, n;
+char *tmp;
+va_list args;
+if (!format)
+{
+printf("\n");
+return;
+}
+n = _strlen((char *) format);
+va_start(args, format);
+i = 0;
+while (i < n)
+{
+if (i > 0 && inTarget(format[i]) == 1)
+printf(", ");
+switch (format[i])
+{
+case 'c':
+printf("%c", va_arg(args, int));
+break;
+case 'i':
+printf("%d", va_arg(args, int));
+break;
+case 'f':
+printf("%f", va_arg(args, double));
+break;
+case 's':
+tmp = va_arg(args, char *);
+printf("%s", tmp == NULL ? "(nil)" : tmp);
+break;
+default:
+break;
+}
+i++;
+}
+ending(args);
 }
