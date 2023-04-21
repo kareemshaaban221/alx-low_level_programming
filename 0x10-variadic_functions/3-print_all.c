c11776022f8e08561803ec0506c4503d0b2f4b3f
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-04-21 23:51:49 +0200

    test

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,6 +2,24 @@
 #include <stdarg.h>
 
 /**
+ * _strlen - function
+ * @s: pointer
+ * Return: int len
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
+
+/**
  * ending - sum args from variadic args
  * @args: string to print
  * Return: No @return value
@@ -13,45 +31,48 @@ va_end(args);
 }
 
 /**
+ * inTarget - sum args from variadic args
+ * @c: string to print
+ * Return: int
+*/
+int inTarget(char c)
+{
+    return c == 'c' || c == 'i' || c == 'f' || c == 's' ? 1 : 0;
+}
+
+/**
  * print_all - sum args from variadic args
  * @format: string to print
  * Return: No @return value
 */
 void print_all(const char * const format, ...)
 {
-unsigned int i, n = 0;
-char *f = format;
-va_list args;
-while (f != '\0')
-{
-if (*f == 'c' || *f == 'i' || *f == 'f' || *f == 's')
-n++;
-f++;
-}
-va_start(args, n);
-i = 0;
-while (i < n)
-{
-switch (format[i])
-{
-case 'c':
-printf("%c", va_arg(args, char));
-break;
-case 'i':
-printf("%d", va_arg(args, int));
-break;
-case 'f':
-printf("%f", va_arg(args, float));
-break;
-case 's':
-printf("%s", va_arg(args, char *));
-break;
-default:
-break;
-}
-i++;
-if (i < n)
-printf(", ");
-}
-ending(args);
+    unsigned int i, n = _strlen((char *) format);
+    va_list args;
+    va_start(args, format);
+    i = 0;
+    while (i < n)
+    {
+        if (i > 0 && inTarget(format[i]) == 1)
+            printf(", ");
+        switch (format[i])
+        {
+            case 'c':
+                printf("%c", va_arg(args, int));
+                break;
+            case 'i':
+                printf("%d", va_arg(args, int));
+                break;
+            case 'f':
+                printf("%f", va_arg(args, double));
+                break;
+            case 's':
+                printf("%s", va_arg(args, char *));
+                break;
+            default:
+                break;
+        }
+        i++;
+    }
+    ending(args);
 }
