9fdeefd50da0eae95365df99d6124cd09ac4d0d9
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-04-27 21:34:49 +0200

    test

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -7,15 +7,15 @@
  */
 int _strlen(const char *s)
 {
-int len = 0;
+    int len = 0;
 
-while (*s != '\0')
-{
-len++;
-s++;
-}
+    while (*s != '\0')
+    {
+        len++;
+        s++;
+    }
 
-return (len);
+    return (len);
 }
 
 /**
@@ -23,26 +23,28 @@ return (len);
  * @head: ajdgopa
  * @str: ajgpo
  * Return: List_t
-*/
+ */
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *res;
-list_t *h = *head;
+    list_t *res;
+    list_t *h;
 
-res = malloc(sizeof(list_t));
-if (!res)
-return (NULL);
+    res = malloc(sizeof(list_t));
+    if (!res)
+        return (NULL);
 
-res->str = strdup(str);
-res->next = NULL;
-res->len = _strlen(str);
+    res->str = strdup(str);
+    res->next = NULL;
+    res->len = _strlen(str);
 
-while (h)
-{
-h = h->next;
-}
-
-h = res;
-
-return (res);
+    if (!head)
+        *head = res;
+    else
+    {
+        h = *head;
+        *head = res;
+        res->next = h;
+    }
+    
+    return (res);
 }
