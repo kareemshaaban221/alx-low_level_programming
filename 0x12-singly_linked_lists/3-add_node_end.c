dd5a48993ebcfc4f14c5ec9602f31f21a413fde7
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-04-28 02:53:34 +0300

    test

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -7,38 +7,45 @@
  */
 int _strlen(const char *s)
 {
-int len = 0;
+    int len = 0;
 
-while (*s != '\0')
-{
-len++;
-s++;
+    while (*s != '\0')
+    {
+        len++;
+        s++;
+    }
+
+    return (len);
 }
 
-return (len);
-}
-
+/**
+ * add_node_end - ksjof
+ * @head: aosd
+ * @str: aspogd
+ * Return: ojadsp
+*/
 list_t *add_node_end(list_t **head, const char *str)
 {
-list_t *res;
-list_t *h;
+    list_t *res;
+    list_t *h;
 
-res = malloc(sizeof(list_t));
-if (!res)
-return (NULL);
+    res = malloc(sizeof(list_t));
+    if (!res)
+        return (NULL);
 
-res->str = strdup(str);
-res->next = NULL;
-res->len = _strlen(str);
+    res->str = strdup(str);
+    res->next = NULL;
+    res->len = _strlen(str);
 
-if (!head)
-*head = res;
-else
-{
-h = *head;
-while (h->next)
-h = h->next;
-h->next = res;
+    if (!head)
+        *head = res;
+    else
+    {
+        h = *head;
+        while (h)
+            h = h->next;
+        h = res;
+    }
+
+    return (res);
 }
-
-return (res);
