e3d9d5910680f42fdbd31e3be80957475d0eb834
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-04-28 04:37:35 +0300

    test

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,11 @@
 #include "lists.h"
 
+/**
+ * first - agkjodsf
+*/
 void first(void)
 {
-printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+printf(
+"You're beat! and yet, you must allow,\nI bore my house upon my back!\n"
+);
 }
