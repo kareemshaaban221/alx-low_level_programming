4e6dd1b8bb67e090f211637440d204b4bbd5f0b9
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-04-28 03:27:42 +0300

    test

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,11 +1,18 @@
 #include "lists.h"
 
+/**
+ * free_list - ajgd
+ * @head: agjd
+ * Return: void
+*/
 void free_list(list_t *head)
 {
-    if (!head) /* base condition */
-        free(head);
-    else
-    {
-        free_list(head->next);
-    }
+if (!head) /* base condition */
+{
+free(head);
+return;
+}
+
+free_list(head->next);
+free(head);
 }
