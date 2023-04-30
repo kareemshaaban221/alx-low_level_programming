5d4865dda1bf89859239e1dc4327fafd49104ca6
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-04-30 04:34:56 +0300

    test

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free a list
+ * @head: apiofjdp
+ * Return: void
+*/
+void free_listint2(listint_t **head)
+{
+listint_t *tmp;
+
+if ((*head))
+{
+while ((*head))
+{
+tmp = (*head);
+(*head) = (*head)->next;
+free(tmp);
+}
+}
+
+head = NULL;
+}
