fa872fdd3cae02888799a26a44b1dc515ab21945
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-04-30 04:43:35 +0300

    test

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * pop_listint - agjsdo
+ * @head: asdighja
+ * Return: aodfjpo
+*/
+int pop_listint(listint_t **head)
+{
+int res = 0;
+listint_t *tmp;
+
+if (!head || *head == NULL)
+return (res);
+
+res = (*head)->n;
+tmp = (*head);
+(*head) = (*head)->next;
+free(tmp);
+}
