143bd273adaa9a4f9546ee0cfebab8c53a34005a
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-05-01 03:09:31 +0300

    test

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - akfhds
+ * @head: aopgjd
+ * @index: aopjdg
+ * Return: agod
+*/
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+int i = 0;
+
+if (!head)
+return (NULL);
+
+while (head)
+{
+if (i == index)
+return (head);
+
+head = head->next;
+}
+
+return (NULL);
+}
