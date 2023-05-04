2c13a620fe9130fdf84937d81ee926d4c7aacfbc
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-05-04 20:54:58 +0300

    test

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,7 +14,7 @@ return (-1);
 
 while (*s != '\0')
 {
-if (*s < '0' || *s > '1')
+if (*s != '0' && *s != '1')
 return (-1);
 len++;
 s++;
@@ -24,31 +24,13 @@ return (len);
 }
 
 /**
- *_pow_recursion - Computes the value of @x raised to the powe
-*@x: The base integer.
-*@y: The exponent integer.
-*Return: The factorial of the integer if @n is non-negative. If @n is negative
-*returns -1 to indicate an error.
-*/
-uint _pow_recursion(int x, int y)
-{
-if (y < 0)
-return (-1);
-
-if (y == 0)
-return (1);
-
-return (x * _pow_recursion(x, y - 1));
-}
-
-/**
  * binary_to_uint - anfpdi
  * @b: afdo
  * Return: fofds
 */
 unsigned int binary_to_uint(const char *b)
 {
-uint res = 0, max;
+uint res = 0, min = 1;
 int i = 0;
 int len = _strlen((char *) b);
 
@@ -58,14 +40,10 @@ return (0);
 if (len == 0)
 return (0);
 
-if (len == 1)
-return (*b - '0');
-
-max = _pow_recursion(2, len - 1);
-for (; i < len; i++)
+for (i = len - 1; i >= 0; i--)
 {
-res += max * (b[i] - '0');
-max /= 2;
+res += min * (b[i] - '0');
+min *= 2;
 }
 
 return (res);
