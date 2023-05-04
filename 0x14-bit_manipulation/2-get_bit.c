cf9db9fcfdf8ca041d4cdcfc788473a6b2a9ce2e
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-05-04 22:15:37 +0300

    test

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -5,25 +5,11 @@
  * @n: adfojg
  * @index: agdjo
  * Return: int
-*/
+ */
 int get_bit(unsigned long int n, unsigned int index)
 {
-uint i;
-
-if (n == 0)
-{
-if (index == 0)
-return (0);
-else
+if (index > 64)
 return (-1);
-}
 
-for (i = 0; i <= index && n > 0; i++)
-{
-if (index == i)
-return (n & 1);
-n >>= 1;
-}
-
-return (-1);
+return ((n >> index) & 1);
 }
