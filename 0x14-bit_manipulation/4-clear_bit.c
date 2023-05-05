37cf1c7a8f2ef0ebd7314d07532f5e8fd9f35d30
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-05-05 03:13:16 +0300

    test

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ *_pow_recursion - Computes the value of @x raised to the powe
+*@x: The base integer.
+*@y: The exponent integer.
+*Return: The factorial of the integer if @n is non-negative. If @n is negative
+*returns -1 to indicate an error.
+*/
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return (-1);
+
+if (y == 0)
+return (1);
+
+return (x * _pow_recursion(x, y - 1));
+}
+
+/**
+ * clear_bit - sgjaopsd
+ * @n: ajdpo
+ * @index: adskpgo
+ * Return: int
+*/
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+short bit;
+
+if (!n)
+return (-1);
+
+if (index > 64)
+return (-1);
+
+bit = (short) (((*n) >> index) & (1));
+
+if (bit == 1)
+*n -= _pow_recursion(2, index);
+
+return (1);
+}
