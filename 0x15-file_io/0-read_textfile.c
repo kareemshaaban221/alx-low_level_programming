fac387cba1c6bf7abcd84160d55360a459f0a3ee
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-05-09 02:38:55 +0300

    test

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,9 +9,14 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 int file, n;
-char buffer[1024];
+char *buffer;
 
 if (!filename)
+return (0);
+
+buffer = malloc(letters + 1);
+
+if (!buffer)
 return (0);
 
 file = open(filename, O_RDONLY);
@@ -21,7 +26,12 @@ return (0);
 
 n = read(file, buffer, letters);
 
-if (write(STDOUT_FILENO, buffer, letters) != (ssize_t) letters)
+if (n == -1)
+return (0);
+
+buffer[n] = '\0';
+
+if (write(STDOUT_FILENO, buffer, n) != (ssize_t) n)
 return (0);
 
 close(file);
