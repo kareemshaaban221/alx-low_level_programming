f6d3c3e6fafafa441d0142a8e04f474f5c19dd3f
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-05-09 04:07:26 +0300

    test

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -24,106 +24,112 @@ return (len);
 }
 
 /**
- * str_concat - concate
- * @s1: the string to concatenate
- * @s2: the string to conc
- * Return: the concatenated string
+ * closeFiles - asdfjago
+ * @f1: apgjd
+ * @f2: adfpjgja
+ * Return: asgjdsa
 */
-char *str_concat(char *s1, char *s2)
-{
-int len1 = _strlen(s1);
-int len2 = _strlen(s2);
-char *res = malloc((len1 + len2 + 1) * sizeof(char));
-int i;
-
-if (res == NULL)
-return (NULL);
-
-for (i = 0; i < len1; i++)
-res[i] = s1[i];
-
-for (i = 0; i < len2; i++)
-res[i + len1] = s2[i];
-
-res[len1 + len2] = '\0';
-
-return (res);
-}
-
 void closeFiles(int f1, int f2)
 {
-    if(close(f1) == -1)
-    {
-        dprintf(STDOUT_FILENO, "Error: Can't close fd %d\n", f1);
-        exit(100);
-    }
-    
-    if(close(f2) == -1)
-    {
-        dprintf(STDOUT_FILENO, "Error: Can't close fd %d\n", f2);
-        exit(100);
-    }
+if (close(f1) == -1)
+{
+dprintf(STDOUT_FILENO, "Error: Can't close fd %d\n", f1);
+exit(100);
 }
 
+if (close(f2) == -1)
+{
+dprintf(STDOUT_FILENO, "Error: Can't close fd %d\n", f2);
+exit(100);
+}
+}
+
+/**
+ * openFileCreate - adgskaop
+ * @filename: adsihfap
+ * @f1: ajgois
+ * Return: apgifhsda
+*/
+int openFileCreate(char *filename, int f1)
+{
+int f2;
+
+f2 = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+if (f2 == -1)
+{
+close(f1);
+dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", filename);
+exit(99);
+}
+
+return (f2);
+}
+
+/**
+ * readWrite - adsgjiop
+ * @f1: jgasdpog
+ * @f2: ajdgip
+ * @filename: adfjpoag
+ * Return: adpgia
+*/
+void readWrite(int f1, int f2, char *filename)
+{
+int n, m;
+char buffer[1024];
+
+n = read(f1, buffer, sizeof(buffer));
+if (n == -1)
+{
+closeFiles(f1, f2);
+return (0);
+}
+m = write(f2, buffer, n);
+if (m == -1)
+{
+closeFiles(f1, f2);
+dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", filename);
+exit(99);
+}
+while (buffer[n] != EOF && buffer[n])
+{
+n = read(f1, buffer, sizeof(buffer));
+if (n == 0)
+{
+closeFiles(f1, f2);
+return (0);
+}
+m = write(f2, buffer, n);
+if (m == -1)
+{
+closeFiles(f1, f2);
+dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", filename);
+exit(99);
+}
+}
+}
+
+/**
+ * main - angdsak
+ * @argc: ajdpig
+ * @argv: adiag
+ * Return: asdpgi
+*/
 int main(int argc, char **argv)
 {
-    int f1, f2, n, m;
-    char buffer[1024];
-
-    if (argc != 3)
-    {
-        dprintf(STDOUT_FILENO, "Usage: cp file_from file_to\n");
-        exit(97);
-    }
-
-    f1 = open(argv[1], O_RDONLY);
-    if (f1 == -1)
-    {
-        dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", argv[1]);
-        exit(98);
-    }
-
-    f2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-    if (f2 == -1)
-    {
-        close(f1);
-        dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", argv[2]);
-        exit(99);
-    }
-
-    n = read(f1, buffer, sizeof(buffer));
-    if (n == -1)
-    {
-        closeFiles(f1, f2);
-        return (0);
-    }
-
-    m = write(f2, buffer, n);
-
-    if (m == -1)
-    {
-        closeFiles(f1, f2);
-        dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", argv[2]);
-        exit(99);
-    }
-    
-    while (buffer[n] != EOF && buffer[n])
-    {
-        n = read(f1, buffer, sizeof(buffer));
-        if (n == 0)
-        {
-            closeFiles(f1, f2);
-            return (0);
-        }
-        m = write(f2, buffer, n);
-
-        if (m == -1)
-        {
-            closeFiles(f1, f2);
-            dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", argv[2]);
-            exit(99);
-        }
-    }
-
-    return (0);
+int f1, f2, n, m;
+char buffer[1024];
+if (argc != 3)
+{
+dprintf(STDOUT_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
+}
+f1 = open(argv[1], O_RDONLY);
+if (f1 == -1)
+{
+dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+f2 = openFileCreate(argv[2], f1);
+readWrite(f1, f2, argv[2]);
+return (0);
 }
