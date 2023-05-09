ef669fb11a85c58ae814b272eca50e692fee909f
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-05-09 04:20:56 +0300

    test

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,137 +1,73 @@
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
 
+void check_stat(int stat, int fd, char *filename, char mode);
 /**
- * _strlen - function
- * @s: pointer
- * Return: int len
+ * main - jiagdospga
+ * @argc: hdsoaighisa
+ * @argv: gjsadpoijga
+ *
+ * Return: lgdpjasoigjapos
  */
-int _strlen(char *s)
+int main(int argc, char *argv[])
 {
-char *ss = s;
-int len = 0;
-
-if (s == NULL)
-return (0);
-
-while (*ss != '\0')
-{
-len++;
-ss++;
-}
-
-return (len);
-}
-
-/**
- * closeFiles - asdfjago
- * @f1: apgjd
- * @f2: adfpjgja
- * Return: asgjdsa
-*/
-void closeFiles(int f1, int f2)
-{
-if (close(f1) == -1)
-{
-dprintf(STDOUT_FILENO, "Error: Can't close fd %d\n", f1);
-exit(100);
-}
-
-if (close(f2) == -1)
-{
-dprintf(STDOUT_FILENO, "Error: Can't close fd %d\n", f2);
-exit(100);
-}
-}
-
-/**
- * openFileCreate - adgskaop
- * @filename: adsihfap
- * @f1: ajgois
- * Return: apgifhsda
-*/
-int openFileCreate(char *filename, int f1)
-{
-int f2;
-
-f2 = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-if (f2 == -1)
-{
-close(f1);
-dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", filename);
-exit(99);
-}
-
-return (f2);
-}
-
-/**
- * readWrite - adsgjiop
- * @f1: jgasdpog
- * @f2: ajdgip
- * @filename: adfjpoag
- * Return: adpgia
-*/
-int readWrite(int f1, int f2, char *filename)
-{
-int n, m;
+int f1, f2, n = 1024, m;
+unsigned int mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 char buffer[1024];
-
-n = read(f1, buffer, sizeof(buffer));
-if (n == -1)
-{
-closeFiles(f1, f2);
-return (0);
-}
-m = write(f2, buffer, n);
-if (m == -1)
-{
-closeFiles(f1, f2);
-dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", filename);
-exit(99);
-}
-while (n == 1024)
-{
-n = read(f1, buffer, sizeof(buffer));
-if (n == 0)
-{
-closeFiles(f1, f2);
-return (0);
-}
-m = write(f2, buffer, n);
-if (m == -1)
-{
-closeFiles(f1, f2);
-dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", filename);
-exit(99);
-}
-}
-return (0);
-}
-
-/**
- * main - angdsak
- * @argc: ajdpig
- * @argv: adiag
- * Return: asdpgi
-*/
-int main(int argc, char **argv)
-{
-int f1, f2;
 
 if (argc != 3)
 {
-dprintf(STDOUT_FILENO, "Usage: cp file_from file_to\n");
+dprintf(STDERR_FILENO, "%s", "Usage: cp file_from file_to\n");
 exit(97);
 }
 f1 = open(argv[1], O_RDONLY);
-if (f1 == -1)
+check_IO_stat(f1, -1, argv[1], 'O');
+f2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
+check_IO_stat(f2, -1, argv[2], 'W');
+while (n == 1024)
 {
-dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", argv[1]);
+n = read(f1, buffer, sizeof(buffer));
+if (n == -1)
+check_IO_stat(-1, -1, argv[1], 'O');
+m = write(f2, buffer, n);
+if (m == -1)
+check_IO_stat(-1, -1, argv[2], 'W');
+}
+n = close(f1);
+check_IO_stat(n, f1, NULL, 'C');
+m = close(f2);
+check_IO_stat(m, f2, NULL, 'C');
+return (0);
+}
+
+/**
+ * check_IO_stat - jgapsodjgpaojds
+ * @stat: gpjasdopgaj
+ * @filename: gaosdjgpoaga
+ * @mode: ghaoisdhiga
+ * @fd: agsdo[gjapods]
+ *
+ * Return: void
+ */
+void check_stat(int stat, int fd, char *filename, char mode)
+{
+if (mode == 'C' && stat == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+exit(100);
+}
+else if (mode == 'O' && stat == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
 exit(98);
 }
-f2 = openFileCreate(argv[2], f1);
-return (readWrite(f1, f2, argv[2]));
+else if (mode == 'W' && stat == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+exit(99);
+}
 }
