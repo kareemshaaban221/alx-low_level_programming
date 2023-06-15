00a93a8b5bcb06198b2ef4c5fdd73f1cb0a29ef4
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-06-16 00:26:12 +0300

    test

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,7 +11,6 @@ size_t print_dlistint(const dlistint_t *h) {
     int len = 0;
 
     if (!h) {
-        print_size(0);
         return (0);
     }
 
@@ -21,6 +20,5 @@ size_t print_dlistint(const dlistint_t *h) {
         h = h->next;
     }
 
-    print_size(len);
     return (len);
 }
