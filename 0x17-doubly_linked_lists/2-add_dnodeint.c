417ab4fe011bf89d9b7350a64c96d107f3fbb082
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-06-16 00:51:16 +0300

    test

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,31 +1,31 @@
 #include "lists.h"
 
 /**
- * dlistint_len - test
+ * add_dnodeint - test
  * @h: asjdg
  * Return: size_t
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    	dlistint_t *new = NULL;
+dlistint_t *new = NULL;
 
-	new = malloc(sizeof(dlistint_t));
-	if (!new)
-		return (NULL);
-	new->n = n;
-	new->prev = NULL;
+new = malloc(sizeof(dlistint_t));
+if (!new)
+return (NULL);
+new->n = n;
+new->prev = NULL;
 
-	if (*head == NULL)
-	{
-		new->next = NULL;
-		*head = new;
-	}
-	else
-	{
-		new->next = *head;
-		if (*head)
-			(*head)->prev = new;
-		*head = new;
-	}
-	return (new);
+if (*head == NULL)
+{
+new->next = NULL;
+*head = new;
+}
+else
+{
+new->next = *head;
+if (*head)
+(*head)->prev = new;
+*head = new;
+}
+return (new);
 }
