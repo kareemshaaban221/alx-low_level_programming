0777537ff065339cf9983f3e6fc4770dd86aa9d1
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-06-16 00:42:10 +0300

    test

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -16,17 +16,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
     if (!node)
         return (NULL);
     node->n = n;
-    node->prev = NULL;
-    if (!(*head))
-    {
-        node->next = NULL;
-        *head = node;
-    }
-    else
-    {
-        node->next = *head;
-        *head = node;
-    }
+    node->prev = (*head)->prev;
+    node->next = *head;
+    *head = node;
 
     return (node);
 }
