5edfb7563cb6827d08181b996c2d6ca2c96887f2
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-06-16 00:29:48 +0300

    test

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - test
+ * @h: asjdg
+ * Return: size_t
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+    int len = 0;
+
+    if (!h)
+    {
+        return (0);
+    }
+
+    while (h)
+    {
+        len++;
+        h = h->next;
+    }
+
+    return (len);
+}
