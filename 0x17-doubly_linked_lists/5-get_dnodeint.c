d99f32219d46d62ecc7f95b06facf8fde58346e5
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-06-16 01:17:11 +0300

    test

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -9,15 +9,13 @@
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
     unsigned int i = 0;
-
-    if (!head)
-        return (NULL);
     
     while (head)
     {
         if (i == index)
             return (head);
         head = head->next;
+        i++;
     }
 
     return (NULL);
