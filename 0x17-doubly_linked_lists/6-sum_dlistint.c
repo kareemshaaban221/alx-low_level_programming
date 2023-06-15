2022b3e7aa800a1f982087e2baf6810e1a565e48
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-06-16 01:21:30 +0300

    test

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - test
+ * @head: asjdg
+ * Return: int
+ */
+int sum_dlistint(dlistint_t *head)
+{
+    int sum = 0;
+
+    if (!head)
+        return (0);
+    
+    while (head)
+    {
+        sum += head->n;
+        head = head->next;
+    }
+
+    return (sum);
+}
