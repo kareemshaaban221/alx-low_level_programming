47121e9c6131638939e156e3dbb20eea6c761cc9
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-12-22 19:55:06 +0200

    make set element to hash table function

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,18 +14,10 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 unsigned long int index = key_index((const unsigned char *)key, ht->size);
-char *key_str = malloc(sizeof(char *)), *value_str = malloc(sizeof(char *));
 hash_node_t *temp = NULL;
 hash_node_t *to_be_stored = (hash_node_t *)malloc(sizeof(hash_node_t));
 
-if (!to_be_stored || !key_str || !value_str || !value || !key || !(*key))
-return (0);
-
-to_be_stored->key = strcpy(key_str, key);
-to_be_stored->value = strcpy(value_str, value);
-to_be_stored->next = NULL;
-
-if (!to_be_stored->key || !to_be_stored->value)
+if (!to_be_stored || !value || !key || !(*key))
 return (0);
 
 if (!ht->array[index])
@@ -38,13 +30,20 @@ while (temp)
 if (strcmp(temp->key, key) == 0)
 {
 free(temp->value);
-temp->value = to_be_stored->value;
-free(to_be_stored->key);
+temp->value = strdup(value);
+if (!temp->value)
+return (0);
 free(to_be_stored);
 return (1);
 }
 temp = temp->next;
 }
+to_be_stored->key = strdup(key);
+to_be_stored->value = strdup(value);
+to_be_stored->next = NULL;
+
+if (!to_be_stored->key || !to_be_stored->value)
+return (0);
 temp = ht->array[index];
 ht->array[index] = to_be_stored;
 to_be_stored->next = temp;
