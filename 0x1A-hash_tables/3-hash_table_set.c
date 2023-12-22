eb6fb98a359f1950d15e16fe1d2ece71d8ea5dd5
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-12-22 20:25:36 +0200

    make set element to hash table function

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -6,6 +6,7 @@
  * get the ht->array at the index of key
  * create a node with the value
  * append to the node inside the position
+ * ! (ERROR): I was declare index and temp before checking the condition of NULL value
  * @ht: the hash table
  * @key: key string
  * @value: value string
@@ -13,33 +14,34 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int index = 0;
-    hash_node_t *temp = NULL;
-    hash_node_t *to_be_stored = NULL;
+unsigned long int index = 0;
+hash_node_t *temp = NULL;
+hash_node_t *to_be_stored = NULL;
 
-    if (!ht || !key || !(*key) || !value)
-        return (0);
+if (!ht || !key || !(*key) || !value)
+return (0);
 
-    index = key_index((const unsigned char *)key, ht->size);
-    temp = ht->array[index];
+index = key_index((const unsigned char *)key, ht->size);
+temp = ht->array[index];
 
-    while (temp && strcmp(temp->key, key) != 0)
-        temp = temp->next;
-    if (temp)
-    {
-        free(temp->value);
-        temp->value = strdup(value);
-        return (1);
-    }
+while (temp && strcmp(temp->key, key) != 0)
+temp = temp->next;
 
-    to_be_stored = (hash_node_t *) malloc(sizeof(hash_node_t));
-    to_be_stored->key = strdup(key);
-    to_be_stored->value = strdup(value);
-    to_be_stored->next = NULL;
+if (temp)
+{
+free(temp->value);
+temp->value = strdup(value);
+return (1);
+}
 
-    temp = ht->array[index];
-    ht->array[index] = to_be_stored;
-    to_be_stored->next = temp;
+to_be_stored = (hash_node_t *) malloc(sizeof(hash_node_t));
+to_be_stored->key = strdup(key);
+to_be_stored->value = strdup(value);
+to_be_stored->next = NULL;
 
-    return (1);
+temp = ht->array[index];
+ht->array[index] = to_be_stored;
+to_be_stored->next = temp;
+
+return (1);
 }
