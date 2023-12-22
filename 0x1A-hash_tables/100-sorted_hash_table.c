beed2b514ebc2321e007a322192dc29860383f27
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-12-22 23:29:16 +0200

    new: making a sorted hash table

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -128,7 +128,6 @@ char *shash_table_get(const shash_table_t *ht, const char *key)
  */
 void shash_table_print(const shash_table_t *ht)
 {
-    unsigned long int i = 0;
     shash_node_t *node = NULL;
     short printed = 0;
 
@@ -185,7 +184,6 @@ void shash_table_delete(shash_table_t *ht)
  */
 void shash_table_print_rev(const shash_table_t *ht)
 {
-    unsigned long int i = 0;
     shash_node_t *node = NULL;
     short printed = 0;
 
